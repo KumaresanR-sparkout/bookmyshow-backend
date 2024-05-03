d2079fc7420d9d081897e643fc57374d28215c79
KumaresanR-sparkout <kumaresan@sparkouttech.com>
2024-05-03 18:32:17 +0530

    changes in listingmovies

--- src/controllers/cityWiseReleaseMovie-controller.js
+++ src/controllers/cityWiseReleaseMovie-controller.js
@@ -6,7 +6,7 @@ export const cityWiseAllReleaseMovie = async (req, res) => {
         console.log(req.params.city)
         const cityWiseMovieLists = await Movies.find()
             .populate({ path: 'screenRef', match: { city: { $eq: req.params.city } }, select: ['screen'] }).exec()
-        sendSuccessResponse(res, 200, "listing all movies in cinemas", cityWiseMovieLists)
+        sendSuccessResponse(res, 200, "listing all movies in selected city", cityWiseMovieLists)
     }
     catch (error) {
         sendErrorResponse(res, 500, error.message)
--- src/controllers/listAllMovie-controller.js
+++ src/controllers/listAllMovie-controller.js
@@ -4,7 +4,7 @@ import { sendSuccessResponse, sendErrorResponse } from '../utils/commonResponse-
 export const getMovieLists = async (req, res) => {
     try {
         const movieLists = await Movies.find().populate('screenRef').exec()
-        sendSuccessResponse(res, 200, "listing all movies in select city", movieLists)
+        sendSuccessResponse(res, 200, "listing all movies in cinemas", movieLists)
     }
     catch (error) {
         sendErrorResponse(res, 500, error.message)
