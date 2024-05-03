7bb31bdb6ce6461ddb81a4fb500fa4bf7af9d579
KumaresanR-sparkout <kumaresan@sparkouttech.com>
2024-05-03 10:46:21 +0530

    city listing and citywise running movie list endpoint added

---
+++ src/controllers/cityWiseReleaseMovie-controller.js
@@ -0,0 +1,40 @@
+import moviesModel from '../models/movies.model'
+import Movies from '../models/movies.model'
+import MovieScreen from '../models/screen.model'
+
+export const cityWiseAllReleaseMovie = async (req, res) => {
+    try {
+
+        const listCity = req.params.id
+        console.log(listCity)
+        const movieList = await MovieScreen.aggregate([
+            {
+                $match: { city: req.params.city }
+            },
+            {
+                $lookup: {
+                    from: "movies",
+                    localField: "screenId",
+                    foreignField: "screenId",
+                    as: "listMovies"
+                }
+
+            },
+            {
+                $project: {
+                    "listMovies.movieId": 1,
+                    "listMovies.movieName": 1,
+                    "listMovies.language":1,
+                    "listMovies.time":1,
+                    "listMovies.types":1,
+                    "listMovies.release_date":1
+                }
+            }
+        ])
+        res.send(movieList)
+    }
+    catch (error) {
+        res.send(error.message)
+    }
+}
+
--- src/routes/movie-route.js
+++ src/routes/movie-route.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import { saveMovieList } from '../controllers/movieLists-controller'
 import { getMovieLists } from '../controllers/listAllMovie-controller'
-import {verifyToken} from '../middlewares/jwtTokenVerification-middleware'
-import {getAllLocations} from '../controllers/location-controller'
+import { verifyToken } from '../middlewares/jwtTokenVerification-middleware'
+import { getAllLocations } from '../controllers/location-controller'
+import { cityWiseAllReleaseMovie } from '../controllers/cityWiseReleaseMovie-controller'
 
 const router = express.Router()
 router.use(express.json())
 
-router.post('/',verifyToken, saveMovieList)
+router.post('/', verifyToken, saveMovieList)
 router.get('/', getMovieLists)
-router.get('/locations',getAllLocations)
-
+router.get('/locations', getAllLocations)
+router.get('/:city', cityWiseAllReleaseMovie)
 
 export default router
